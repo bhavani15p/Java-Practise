d0a3dae5cfdd757c30883349c34ca4d415da2e5f
Bhavani P <119038948+bhavani15p@users.noreply.github.com>
2023-07-24 11:11:02 +0530

    Add files via upload

---
+++ Practised Java Concepts/DSA/Sorting/inbuiltSort.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class inbuiltSort {
+
+    public static void main(String[] args) {
+        int arr[] = { 9, 8, 6, 4, 5, 2, 1, 7, 3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    
+}
